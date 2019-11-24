65bcb9c5ce1683e62a6b4e7b225320ddf1714725
andikalaksana30 <andikalaksana2000@gmail.com>
2019-11-24 22:46:11 +0700

    versi 1.0.0

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.main')
+
+@section('title', 'Web Mahasiswa')
+
+@section('container')
+  <div class="container">
+    <div class="row">
+      <div class="col-10">
+        <h1 class="mt-3">Halaman Contoh, Halaman Utama (Aplikasi CRUD) ada di NavBar Students</h1>
+      </div>
+    </div>
+  </div>
+@endsection
+
+    
+
---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.main')
+
+@section('title', 'Daftar Mahasiswa')
+
+@section('container')
+  <div class="container">
+    <div class="row">
+      <div class="col-10">
+        <h1 class="mt-3">Halaman Contoh/h1>
+        
+
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quos quis et, suscipit fuga ipsum adipisci quas eveniet similique repellendus non amet expedita, nisi quidem ad delectus voluptas dignissimos magnam!</p>
+    </div>
+  </div>
+@endsection
+
+    
+
